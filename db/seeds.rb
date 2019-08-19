709b82e924b49e3db08b77372df4645444224a69
Mathieu Robin <mathieu.robin1@gmail.com>
2019-08-19 17:43:20 +0200

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    Car.destroy_all
+    puts "destroy all cars"
+
+    User.create!(first_name: "Juan", last_name: "Limo Flores", user_name: "juanlf", email: "juancarlos.limo@hotmail.com", password: "admin123")
+
+    Car.create!(user_id:2, brand:"Jaguar", model:"E-type", origin:"British", year:1962, location:"Berlin")
+    Car.create!(user_id:2, brand:"Citroen", model:"DS", origin:"French", year:1963,location:"Berlin")
+    Car.create!(user_id:2, brand:"Mercedes", model:"300 SL", origin:"German", year:1955,location:"Berlin")
+
+    puts "created cars"
+
+
