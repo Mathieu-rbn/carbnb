3607f4bbeab765fe9d233bf3a6a48162ea681b70
juanca limo <juancarlos.limo@hotmail.com>
2019-08-20 15:08:56 +0200

    new features: login and user list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
 
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   # get 'cars'    , to: 'cars#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cars, only: [:index, :show]
+  resources :users, only: [:show]
 end
 
 
