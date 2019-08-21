59583fc648c7a95c4c543a5a79229f175efd506c
juanca limo <juancarlos.limo@hotmail.com>
2019-08-21 15:55:49 +0200

    new files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-    Car.destroy_all
-    puts "destroy all cars"
-
-    User.create!(first_name: "Juan", last_name: "Limo Flores", user_name: "juanlf", email: "juancarlos.limo@gmail.com", password: "admin123")
-    User.create!(first_name: "Juergen", last_name: "Nguyen", user_name: "jungustar", email: "juergen.nguyen@gmail.com", password: "admin123")
-
-    Car.create!(user_id: User.first.id, brand:"Jaguar", model:"E-type", origin:"British", year:1962, location:"Berlin")
-    Car.create!(user_id: User.first.id, brand:"Citroen", model:"DS", origin:"French", year:1963,location:"Berlin")
-    Car.create!(user_id: User.first.id, brand:"Mercedes", model:"300 SL", origin:"German", year:1955,location:"Berlin")
-
-    puts "created cars"
 
 
+Booking.destroy_all
+Car.destroy_all
+User.destroy_all
+puts "destroy all cars"
+User.create!(
+  first_name: "Juan",
+  last_name: "Limo Flores",
+  user_name: "juanlf",
+  email: "juancarlos.limo@gmail.com",
+  password: "admin123"
+)
+
+User.create!(first_name: "Juergen", last_name: "Nguyen", user_name: "jungustar", email: "juergen.nguyen@gmail.com", password: "admin123")
+Car.create!(user_id: User.last.id, brand:"Aston Martin", model:"DB5", origin:"British", year:1963, location:"Berlin", remote_photo_url: "https://res.cloudinary.com/mathieurobin1/image/upload/v1566333620/Aston_Martin_DB5_tnmdi5.jpg")
+Car.create!(user_id: User.last.id, brand:"Citroen", model:"DS", origin:"French", year:1963, location:"Berlin", remote_photo_url:"https://res.cloudinary.com/mathieurobin1/image/upload/v1566334028/Citroen_DS_m1gncg.jpg")
+Car.create!(user_id: User.last.id, brand:"Mercedes", model:"300 SL", origin:"German", year:1955, location:"Berlin", remote_photo_url:"https://res.cloudinary.com/mathieurobin1/image/upload/v1566333620/mercedes-300-sl-web_mdi53p.jpg")
+puts "created cars"
